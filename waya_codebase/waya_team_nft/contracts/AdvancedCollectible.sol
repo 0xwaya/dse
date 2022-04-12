0d372f9b157f78c67b23aba07fc9962f19d032fd
e.mercado <admin@wayalabs.com>
2022-04-12 10:01:12 -0400

    commit to master

--- waya_codebase/waya_team_nft/contracts/AdvancedCollectible.sol
+++ waya_codebase/waya_team_nft/contracts/AdvancedCollectible.sol
@@ -1,7 +1,7 @@
-pragma solidity 0.6.06;
+pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+import "@chainlink/blob/develop/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract AdvancedCollectible is ERC721, VRFConsumerBase {
     uint256 public tokenCounter;
--- waya_codebase/waya_team_nft/contracts/WayaTeamWaca.sol
+++
@@ -1,6 +0,0 @@
- pragma solidity 0.6.6
-
- import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
- import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
-
-contract WayaTeamWaca
