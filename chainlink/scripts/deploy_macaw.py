da943d7657f25ee6d53aaabbc24ec5e008c0ddfc
e.mercado <admin@wayalabs.com>
2022-11-27 09:39:13 -0500

    commit to main

--- chainlink/scripts/deploy_macaw.py
+++ chainlink/scripts/deploy_macaw.py
@@ -1,6 +1,22 @@
-from brownie import MacawCollectible, accounts, network, config # imports the  MacawCollectible contract    
+#!/usr/bin/python3
+import os
+from webbrowser import MacOSX
+from brownie import MacawCollectible as brownie_MacawCollectible, accounts, network, config # imports the MacawCollectible contract
+
 
 def main(): # main function
     dev = accounts.add(config["wallets"]["from_key"]) # add the dev account to the accounts list
-    print(dev)
+    print(network.show_active()
+    return MacawCollectible.deploy(config["networks"][network.show_active()]
+                                                        ["vrf_coordinator"], 
+                                   config["networks"][network.show_active()]
+                                                        ["link_token"],
+                                   config["networks"][network.show_active()]
+                                                        ["keyhash"],
+                                      {"from": dev})
+    
+
+        
+                                                        
+                                   
         
